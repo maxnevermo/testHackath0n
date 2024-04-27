66138b416d45b6a0f6179bdd3c84e61ad1ba1862
mykolakraievskyi <mykola.kraievskyi.pz.2022@lpnu.ua>
2024-04-28 02:38:41 +0300

    added the functions for chat functionality

---
+++ controllers/messages-controller.js
@@ -0,0 +1,15 @@
+const getMessages = async (chatroomCollection, messageCollection, req, res) => {
+  try {
+    const chatroomName = req.params.chatroomName;
+    const chatroom = await chatroomCollection.findOne({ roomName: chatroomName });
+    if (!chatroom) {
+      return res.json({ status: "error", message: "Chatroom not found" });
+    }
+    const messages = await messageCollection.find({ room: chatroom.roomName });
+    const room = await chatroomCollection.find({ roomName: chatroomName });
+    res.json({ status: "success", messages: messages, room: room });
+  } catch (err) {
+    console.error(err);
+    res.json({ status: "error", message: "Internal server error" });
+  }
+}
